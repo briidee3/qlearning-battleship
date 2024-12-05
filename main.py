b60586a018a5e3cea3391c245148d60d425f41d9
briidee3 <briidee3@gmail.com>
2024-12-05 12:47:27 -0500

    temp setup for running one game at a time for testing purposes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from game import game, create_random_opponent
+
 agent_wins = 0
 monte_wins = 0
-for i in  range(100):
-  g = game(8,5, 'agent','random')
-  if g.start() == 1:
-    agent_wins += 1
-  else:
-    monte_wins += 1
+#for i in  range(100):
+g = game(8,5, 'agent','random')
+g.start()
+#  if g.start() == 1:
+#    agent_wins += 1
+#  else:
+#    monte_wins += 1
 print("The q-agent won",agent_wins,"percent of the time.")
 print("The random agent won",monte_wins,"percent of the time.")
