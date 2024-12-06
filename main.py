e85256e24464854596b8aa22165e313e6a006704
briidee3 <briidee3@gmail.com>
2024-12-06 10:53:27 -0500

    negated printout during eval, now printing more stats at end

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from game import game, create_random_opponent
+import sys,os
 
 agent_wins = 0
 monte_wins = 0
-#for i in  range(100):
-g = game(8,5, 'agent','random')
-g.start()
-#  if g.start() == 1:
-#    agent_wins += 1
-#  else:
-#    monte_wins += 1
-print("The q-agent won",agent_wins,"percent of the time.")
-print("The random agent won",monte_wins,"percent of the time.")
+num_games = 1000
+
+# send print to os.devnull, essentially turning off print statements from our view
+init_stdout = sys.stdout
+sys.stdout = open(os.devnull, "w")
+
+for i in range(num_games):
+  g = game(8,5, 'agent','random')
+  winner = g.start()
+  if winner == 1:
+    agent_wins += 1
+  else:
+    monte_wins += 1
+
+# send print back to console
+sys.stdout = init_stdout
+
+print("\nUsing empty q-tables:")
+print("\tThe q-agent won",(agent_wins / num_games) % 0.01,"percent of the time.")
+print("\tThe random agent won",(monte_wins / num_games) % 0.01,"percent of the time.\n")
+print("\n\tNumber of q-agent wins:\t%d\n\tNumber of games:\t%d" % (agent_wins, num_games))
