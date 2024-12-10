6276de4b350b44f5a0c1f681f5a09541b970fbcf
briidee3 <briidee3@gmail.com>
2024-12-09 20:13:47 -0500

    comments

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ np.random.seed(int(time.time() % 1000000))
 
 agent_wins = 0
 monte_wins = 0
-num_games = 10000
+num_games = 100
 turns_taken = [0, 0]
 
 # send print to os.devnull, essentially turning off print statements from our view
 init_stdout = sys.stdout
-#sys.stdout = open(os.devnull, "w")
+sys.stdout = open(os.devnull, "w")
 
 for i in range(num_games):
   g = game(8,5, 'agent','random', seed = np.random.randint(0, 99999999))
@@ -26,9 +26,9 @@ for i in range(num_games):
     monte_wins += 1
     turns_taken[1] += cur_turns_taken
   np.random.seed(np.random.randint(0, 999999999))   # set new seed for next run
-  print(g.turn)
-  print(g.player1.table_player.num_shots)
-  print(g.player1.table_player.num_hits)
+  #print(g.turn)
+  #print(g.player1.table_player.num_shots)
+  #print(g.player1.table_player.num_hits)
 
 # send print back to console
 sys.stdout = init_stdout
@@ -38,3 +38,5 @@ print("\tThe q-agent won {:10.2f} percent of the time.".format(100 * (agent_wins
 print("\tThe random agent won {:10.2f} percent of the time.\n".format(100 * (monte_wins / num_games)))
 print("\n\tNumber of q-agent wins:\t%d\n\tNumber of games:\t%d\n" % (agent_wins, num_games))
 print("\nAvg number of turns:\n\tQ-agent: {:10.2f}\n\tRandom: {:10.2f}".format((turns_taken[0] / agent_wins), (turns_taken[1] / monte_wins)))
+print("Num wins p1: %d\nNum turns p1: %d" % (agent_wins, turns_taken[0]))
+print("Num wins p2: %d\nNum turns p2: %d" % (monte_wins, turns_taken[1]))
